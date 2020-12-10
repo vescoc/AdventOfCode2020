3367f0a05044f0725d0ad9e2a71dab0e1fa832e3
vescoc <claudio.vesco@gmail.com>
2020-12-10 19:14:31 +0100

    day10 part 2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -4,6 +4,9 @@ extern crate test;
 #[macro_use]
 extern crate lazy_static;
 
+use itertools::Itertools;
+use std::collections::HashSet;
+
 lazy_static! {
     static ref INPUT: Vec<u32> = parse(include_str!("../input"));
 }
@@ -31,8 +34,53 @@ fn solve_1(input: &[u32]) -> [u32; 3] {
     [a + 1, b, c + 1]
 }
 
-fn solve_2(_input: &[u32]) -> u32 {
-    todo!()
+fn solve_2_r(adapters: &[u32], removed: HashSet<usize>) -> usize {
+    let filtered = || {
+        adapters
+            .iter()
+            .enumerate()
+            .filter_map(|(i, v)| if removed.contains(&i) { None } else { Some(v) })
+    };
+
+    if adapters.len() < 2 {
+        1
+    } else if !filtered().tuple_windows().all(|(a, b)| b - a <= 3) {
+        0
+    } else {
+        let start_idx = removed.iter().max().unwrap_or(&0) + 1;
+        (start_idx..adapters.len() - 1)
+            .map(|i| {
+                solve_2_r(
+                    adapters,
+                    removed.iter().cloned().chain(std::iter::once(i)).collect(),
+                )
+            })
+            .sum::<usize>()
+            + 1
+    }
+}
+
+fn solve_2(input: &[u32]) -> usize {
+    let mut adapters = input
+        .iter()
+        .copied()
+        .chain(std::iter::once(0))
+        .chain(std::iter::once(input.iter().max().unwrap() + 3))
+        .collect::<Vec<_>>();
+
+    adapters.sort_unstable();
+
+    std::iter::once(0)
+        .chain(adapters.windows(2).enumerate().filter_map(|(i, v)| {
+            if v[1] - v[0] == 3 {
+                Some(i)
+            } else {
+                None
+            }
+        }))
+        .tuple_windows()
+        .map(|(a, b)| solve_2_r(&adapters[a..=b], HashSet::new()))
+        .product()
 }
 
 pub fn part_1() -> u32 {
@@ -41,7 +89,7 @@ pub fn part_1() -> u32 {
     r[0] * r[2]
 }
 
-pub fn part_2() -> u32 {
+pub fn part_2() -> usize {
     solve_2(&INPUT)
 }
 
@@ -50,9 +98,8 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    #[test]
-    fn same_results_part_1_1() {
-        let input = parse(
+    lazy_static! {
+        static ref INPUT_1: Vec<u32> = parse(
             r"16
 10
 15
@@ -65,13 +112,7 @@ mod tests {
 12
 4",
         );
-
-        assert_eq!(solve_1(&input), [7, 0, 5]);
-    }
-
-    #[test]
-    fn same_results_part_1_2() {
-        let input = parse(
+        static ref INPUT_2: Vec<u32> = parse(
             r"28
 33
 18
@@ -104,12 +145,44 @@ mod tests {
 10
 3",
         );
+    }
 
-        assert_eq!(solve_1(&input), [22, 0, 10]);
+    #[test]
+    fn same_results_part_1_1() {
+        assert_eq!(solve_1(&INPUT_1), [7, 0, 5]);
+    }
+
+    #[test]
+    fn same_results_part_1_2() {
+        assert_eq!(solve_1(&INPUT_2), [22, 0, 10]);
+    }
+
+    #[test]
+    fn same_results_part_2_1() {
+        assert_eq!(solve_2(&INPUT_1), 8);
+    }
+
+    #[test]
+    fn same_results_part_2_2() {
+        assert_eq!(solve_2(&INPUT_2), 19208);
+    }
+
+    #[test]
+    fn test_solve_2_r_split() {
+        assert_eq!(
+            solve_2_r(&[1, 4], HashSet::new())
+                * solve_2_r(&[4, 5, 7, 8, 9], HashSet::new()),
+            5
+        );
     }
 
     #[bench]
     fn bench_part_1(b: &mut Bencher) {
         b.iter(part_1);
+    }
+
+    #[bench]
+    fn bench_part_2(b: &mut Bencher) {
+        b.iter(part_2);
     }
 }
