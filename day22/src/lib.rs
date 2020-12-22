c074f09945d548f316c9f1dab96ea8fd8e70f61f
vescoc <claudio.vesco@gmail.com>
2020-12-22 16:26:58 +0100

    day22 part 2

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -4,7 +4,7 @@ extern crate test;
 #[macro_use]
 extern crate lazy_static;
 
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
 lazy_static! {
     static ref INPUT: Data = include_str!("../input").parse().expect("invalid input");
@@ -33,6 +33,85 @@ impl std::str::FromStr for Data {
         };
 
         Ok(Data(parse()?, parse()?))
+    }
+}
+
+enum Winner {
+    Player1,
+    Player2,
+}
+
+struct Game {
+    player_1: VecDeque<u32>,
+    player_2: VecDeque<u32>,
+    history: HashSet<(VecDeque<u32>, VecDeque<u32>)>,
+}
+
+impl Game {
+    fn new(player_1: &[u32], player_2: &[u32]) -> Self {
+        Self {
+            player_1: player_1.iter().copied().collect(),
+            player_2: player_2.iter().copied().collect(),
+            history: HashSet::new(),
+        }
+    }
+
+    fn play(&mut self) -> (Winner, &mut [u32]) {
+        use Winner::*;
+
+        loop {
+            {
+                let k = (self.player_1.to_owned(), self.player_2.to_owned());
+                if self.history.contains(&k) {
+                    break (Player1, self.player_1.make_contiguous());
+                } else {
+                    self.history.insert(k);
+                }
+            }
+
+            match (self.player_1.pop_front(), self.player_2.pop_front()) {
+                (Some(card_1), Some(card_2))
+                    if (card_1 as usize) <= self.player_1.len()
+                        && (card_2 as usize) <= self.player_2.len() =>
+                {
+                    match Game::new(
+                        &self.player_1.make_contiguous()[0..card_1 as usize],
+                        &self.player_2.make_contiguous()[0..card_2 as usize],
+                    )
+                    .play()
+                    {
+                        (Player1, _) => {
+                            self.player_1.push_back(card_1);
+                            self.player_1.push_back(card_2);
+                        }
+                        (Player2, _) => {
+                            self.player_2.push_back(card_2);
+                            self.player_2.push_back(card_1);
+                        }
+                    }
+                }
+                (Some(card_1), Some(card_2)) if card_1 > card_2 => {
+                    self.player_1.push_back(card_1);
+                    self.player_1.push_back(card_2);
+                }
+                (Some(card_1), Some(card_2)) if card_1 < card_2 => {
+                    self.player_2.push_back(card_2);
+                    self.player_2.push_back(card_1);
+                }
+                (Some(card), None) => {
+                    self.player_1.push_front(card);
+                    break (Player1, self.player_1.make_contiguous());
+                }
+                (None, Some(card)) => {
+                    self.player_2.push_front(card);
+                    break (Player2, self.player_2.make_contiguous());
+                }
+                u => unreachable!(format!(
+                    "c: {:?} p1: {:?} p2: {:?}",
+                    u, self.player_1, self.player_2
+                )),
+            }
+        }
     }
 }
 
@@ -76,15 +155,25 @@ fn solve_1(input: &Data) -> usize {
         .sum()
 }
 
-fn solve_2(_input: &Data) -> u32 {
-    todo!()
+fn solve_2(input: &Data) -> usize {
+    let mut game = Game::new(&input.0, &input.1);
+
+    let (_, cards) = game.play();
+
+    cards.reverse();
+
+    cards
+        .iter()
+        .enumerate()
+        .map(|(i, v)| *v as usize * (i + 1))
+        .sum()
 }
 
 pub fn part_1() -> usize {
     solve_1(&INPUT)
 }
 
-pub fn part_2() -> u32 {
+pub fn part_2() -> usize {
     solve_2(&INPUT)
 }
 
@@ -118,7 +207,7 @@ Player 2:
 
     #[test]
     fn same_results_part_2() {
-        todo!();
+        assert_eq!(solve_2(&INPUT), 291);
     }
 
     #[bench]
